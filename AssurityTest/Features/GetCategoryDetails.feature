7c67a6db2cf05e3df08db2a45a58572e771eb50d
Keith <peralta.keit@outlook.com>
2021-03-25 16:08:55 +1300

    Add feature file and step definition

---
+++ AssurityTest/Features/GetCategoryDetails.feature
@@ -0,0 +1,10 @@
+﻿Feature: GetCategoryDetails
+	Get category details from the API
+
+@mytag
+Scenario: Successfully Get and Verify Contents for the Categories API
+	Given I call the GET categories endpoint "v1/Categories/6327/Details.json?catalogue=false"
+	Then I should get a response with status code 200
+	And the Name in the response should be "Carbon credits"
+	And CanRelist value in the response should be "true"
+	And the Promotions element with the name "Gallery" should have a Description that contains the text "2x larger image"
---
+++ AssurityTest/StepDefinitions/GetCategoryDetailsSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace AssurityTest.StepDefinitions
+{
+    [Binding]
+    public class GetCategoryDetailsSteps
+    {
+        [Given(@"I call the GET categories endpoint ""(.*)""")]
+        public void GivenICallTheGETCategoriesEndpoint(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should get a response with status code (.*)")]
+        public void ThenIShouldGetAResponseWithStatusCode(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the Name in the response should be ""(.*)""")]
+        public void ThenTheNameInTheResponseShouldBe(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"CanRelist value in the response should be ""(.*)""")]
+        public void ThenCanRelistValueInTheResponseShouldBe(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the Promotions element with the name ""(.*)"" should have a Description that contains the text ""(.*)""")]
+        public void ThenThePromotionsElementWithTheNameShouldHaveADescriptionThatContainsTheText(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
